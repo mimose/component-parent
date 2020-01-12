452fe941f36c4d849371932b000a631871d51139
mimose <602918594@qq.com>
2020-01-12 23:06:38 +0800

    redisson-component  增加静态块MapCache

---
+++ component-redisson-starter/src/main/java/com/mimose/component/redisson/cache/config/DefaultMapCacheConfiguration.java
@@ -0,0 +1,27 @@
+package com.mimose.component.redisson.cache.config;
+
+import com.mimose.component.redisson.cache.util.CacheUtil;
+import com.mimose.component.redisson.common.config.RedissonClientConfiguration;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PostConstruct;
+
+/**
+ * @Description 静态块基本存取配置类
+ * @Author ccy
+ * @Date 2019/12/27
+ */
+@Slf4j
+@Configuration
+@ConditionalOnMissingBean(value = RedissonClientConfiguration.class)
+public class DefaultMapCacheConfiguration {
+
+    @PostConstruct
+    public void afterClient(){
+        log.info("start init default map cache util ...");
+        CacheUtil.setRedissonClient(null);
+        log.info("start init default map cache util success");
+    }
+}
--- component-redisson-starter/src/main/java/com/mimose/component/redisson/cache/util/CacheUtil.java
+++ component-redisson-starter/src/main/java/com/mimose/component/redisson/cache/util/CacheUtil.java
@@ -1,14 +1,16 @@
 package com.mimose.component.redisson.cache.util;
 
-import org.redisson.api.RMap;
 import org.redisson.api.RMapCache;
 import org.redisson.api.RedissonClient;
+import org.springframework.util.ObjectUtils;
 import org.springframework.util.StringUtils;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
@@ -21,7 +23,11 @@ public class CacheUtil {
 
     private static RedissonClient redissonClient;
 
-    private static Map<String, RMap<String, Object>> cacheMap = new HashMap<>();
+    private static Map<String, ConcurrentMap<String, Object>> cacheMap = new HashMap<>();
+
+    private static final Integer INITIAL_CAPACITY = 2<<4;
+
+    private static final String HOLD_TIME = "_HOLD_TIME";
 
     public static void setRedissonClient(RedissonClient redissonClient) {
         CacheUtil.redissonClient = redissonClient;
@@ -63,7 +69,24 @@ public class CacheUtil {
      */
     public static <T> T get(String mapKey, String cacheKey, Class<T> clazz){
         AssertMapKey(mapKey);
-        return containKeys(mapKey, cacheKey)? clazz.cast(cacheMap.get(mapKey).get(cacheKey)) : null;
+        return AfterGet(cacheKey, containKeys(mapKey, cacheKey)? clazz.cast(cacheMap.get(mapKey).get(cacheKey)) : null);
+    }
+
+    /**
+     * 非redis的情况，存在过期存储的情况，需要判断是否过期
+     * @param cacheKey
+     * @param t
+     * @param <T>
+     * @return
+     */
+    private static <T> T AfterGet(String cacheKey, T value) {
+        if(redissonClient == null && !ObjectUtils.isEmpty(value)){
+            Long expiryTime = (Long) cacheMap.get(cacheKey).get(cacheKey + HOLD_TIME);
+            if(System.currentTimeMillis() > expiryTime){
+                value = null;
+            }
+        }
+        return value;
     }
 
     /**
@@ -106,11 +129,11 @@ public class CacheUtil {
      */
     public static void put(String mapKey, String cacheKey, Object value){
         AssertMapKey(mapKey);
-        RMap<String, Object> rMap;
+        ConcurrentMap<String, Object> rMap;
         if(containKeys(mapKey, null)){
             rMap = cacheMap.get(mapKey);
         }else{
-            rMap = redissonClient.getMap(mapKey);
+            rMap = redissonClient == null ? new ConcurrentHashMap<>(INITIAL_CAPACITY) : redissonClient.getMap(mapKey);
             cacheMap.put(mapKey, rMap);
         }
         rMap.put(cacheKey, value);
@@ -126,14 +149,26 @@ public class CacheUtil {
      */
     public static void put(String mapKey, String cacheKey, Object value, Long ttl, TimeUnit unit){
         AssertMapKey(mapKey);
-        RMapCache<String, Object> rMap;
-        if(containKeys(mapKey, null)){
-            rMap = (RMapCache<String, Object>) cacheMap.get(mapKey);
+        if(redissonClient != null){
+            RMapCache<String, Object> rMap;
+            if(containKeys(mapKey, null)){
+                rMap = (RMapCache<String, Object>) cacheMap.get(mapKey);
+            }else{
+                rMap = redissonClient.getMapCache(mapKey);
+                cacheMap.put(mapKey, rMap);
+            }
+            rMap.put(cacheKey, value, ttl, unit);
         }else{
-            rMap = redissonClient.getMapCache(mapKey);
-            cacheMap.put(mapKey, rMap);
+            ConcurrentHashMap<String, Object> cMap;
+            if(containKeys(mapKey, null)){
+                cMap = (ConcurrentHashMap<String, Object>) cacheMap.get(mapKey);
+            }else{
+                cMap = new ConcurrentHashMap<>(INITIAL_CAPACITY);
+                cacheMap.put(mapKey, cMap);
+            }
+            cMap.put(cacheKey, value);
+            cMap.put(cacheKey + HOLD_TIME, System.currentTimeMillis() + unit.toMillis(ttl));
         }
-        rMap.put(cacheKey, value, ttl, unit);
     }
 
     /**
